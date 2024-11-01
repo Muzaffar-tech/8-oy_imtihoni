59c1d7abe8a2ee525d45e67686825cf500c61ad4
Muzaffar-tech <muzaffaromonov10@gmail.com>
2024-11-01 09:02:17 +0500

    initial commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.core import validators
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -37,6 +38,7 @@ class Comment(models.Model):
     '''
     text = models.TextField()
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
